a0a67cf221d5145f1e2c61488462b1952e209652
ming.tang <mzllon@qq.com>
2023-02-22 17:18:26 +0800

    新增了Predicate工具类

---
+++ core/src/main/java/cn/tmkit/core/function/PredicateUtil.java
@@ -0,0 +1,12 @@
+package cn.tmkit.core.function;
+
+/**
+ * {@linkplain java.util.function.Predicate}工具类
+ *
+ * @author ming.tang
+ * @version 0.0.1
+ * @date 2023-02-22
+ */
+public class PredicateUtil extends Predicates {
+
+}
