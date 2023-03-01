5372fa1dac7001c12350025590f0fdc2e0186051
ming.tang <mzllon@qq.com>
2023-03-01 12:35:17 +0800

    更改了模块名统一tmkit-

---
+++ json-api/src/main/java/cn/tmkit/json/api/annotation/JsonProviderName.java
@@ -0,0 +1,33 @@
+package cn.tmkit.json.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * JSON的实现类的名，用于区分不同框架的实现别名
+ *
+ * @author miles.tang
+ * @version 0.0.1
+ * @date 2022-06-06
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface JsonProviderName {
+
+    /**
+     * JSON 厂商名
+     *
+     * @return 厂商名
+     */
+    String value();
+
+    /**
+     * 加载的顺序，值越小越先加载
+     *
+     * @return 加载顺序值
+     */
+    int index() default Integer.MAX_VALUE;
+
+}
