c7d79cc25c3a7862189c3503020ef3ff6fcfaaed
miles.tang <mzllon@qq.com>
2023-03-13 15:06:03 +0800

    增加了几个常见的泛型类型的包装，用于JSON字符串解析为集合时，无需要重复定义类似于如下的重复代码 ```java JSONUtil.fromJson(str, new BaseTypeRef<List<Long>>() {}); ``` 通过便捷方法可以简化使用：`JSONUtil.fromJson(str, BaseTypeRef.LIST_STRING)`

---
+++ sjf4j-api/src/test/java/cn/tmkit/json/sjf4j/util/JSONUtilTest.java
@@ -0,0 +1,57 @@
+package cn.tmkit.json.sjf4j.util;
+
+import cn.tmkit.core.support.Console;
+import cn.tmkit.json.sjf4j.BaseTypeRef;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * Tester for {@linkplain JSONUtil}
+ *
+ * @author ming.tang
+ * @version 0.0.1
+ * @date 2023-03-13
+ */
+public class JSONUtilTest {
+
+    @Test
+    public void typeRef() {
+        class ListLongType extends BaseTypeRef<List<Long>> {
+
+        }
+        ListLongType listLongType = new ListLongType();
+        Type type = listLongType.getType();
+        Console.log(type);
+        Console.log(type.getClass());
+        Console.log(type.getTypeName());
+        Class<? super List<Long>> rawType = listLongType.getRawType();
+        Console.log(rawType);
+    }
+
+    @Test
+    public void toJson() {
+
+    }
+
+    @Test
+    void testToJson() {
+    }
+
+    @Test
+    void testToJson1() {
+    }
+
+    @Test
+    void fromJson() {
+    }
+
+    @Test
+    void testFromJson() {
+    }
+
+    @Test
+    void testFromJson1() {
+    }
+}
